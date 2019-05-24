cb45968bb1a085c9483a56a4819a783be36d027e
Final Child <finalchild2@gmail.com>
2019-05-24 16:42:19 +0900

    Write first fast input implementation (Doesn't run. Why?)

--- src/solution.kt
+++ src/solution.kt
@@ -1,5 +1,8 @@
 import java.io.FilterOutputStream
 import java.io.OutputStream
+import java.io.FilterInputStream
+import java.io.InputStream
+import kotlin.experimental.and
 
 val fastOutStream = run {
     val outField = FilterOutputStream::class.java.getDeclaredField("out")!!
@@ -44,4 +47,52 @@ fun print(s: String) {
 fun println(o: Any?) {
     print(o.toString())
     print("\n")
+}
+
+val fastInStream = run {
+    val inField = FilterInputStream::class.java.getDeclaredField("in")!!
+    inField.isAccessible = true
+    var stream = System.`in` as InputStream
+    while (stream is FilterInputStream) {
+        stream = inField.get(stream) as InputStream
+    }
+    stream
+}
+val fastInBuf = ByteArray(8192)
+var fastInBufPos = 0
+var fastInBufCount = 0
+
+fun fastInFill() {
+    fastInBufPos = 0
+    fastInBufCount = 0
+    val n = fastInStream.read(fastInBuf, fastInBufPos, fastInBuf.size - fastInBufPos)
+    if (n > 0)
+        fastInBufCount = n
+}
+
+fun fastInRead(): Byte {
+    if (fastInBufPos >= fastInBufCount) {
+        fastInFill()
+        if (fastInBufPos >= fastInBufCount)
+            return -1
+    }
+    val result = (fastInBuf[fastInBufPos] and 0xff.toByte())
+    fastInBufPos++
+    return result
+}
+
+fun scanInt(): Int {
+     var c: Byte
+    do {
+        c = fastInRead()
+    } while (c == ' '.toByte() || c == '\n'.toByte());
+
+    var result = c - '0'.toByte()
+    while (true) {
+        c = fastInRead()
+        if (c == (-1).toByte() || c == ' '.toByte() || c == '\n'.toByte()) {
+            return result
+        }
+        result = result * 10 + c - '0'.toByte()
+    }
 }
